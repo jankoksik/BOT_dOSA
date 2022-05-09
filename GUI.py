ebd44ec89a2f90e9f352eddb7ba1de27200bf81a
jankoksik <jawobo123@gmail.com>
2022-05-09 21:26:51 +0200

    almost working gui syn flood

---
+++ GUI.py
@@ -0,0 +1,127 @@
+from asyncio.base_futures import _FINISHED
+from asyncio.windows_events import NULL
+from threading import Thread
+from time import sleep
+
+from kivy.clock import Clock
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.animation import Animation
+from kivy.uix.popup import Popup
+
+
+import modules.auxilliary
+
+
+class MyApp(App):
+    def build(self):
+        return FloatLayouts()
+
+class FloatLayouts(FloatLayout):
+    def CickedOne(self):
+        showPopUp1()
+
+    def CickedTwo(self):
+        showPopUp2()
+
+    def CickedThree(self):
+        showPopUp3()
+
+    def CickedFour(self):
+        showPopUp4()
+
+
+class P1(FloatLayout):
+    Dane = modules.auxilliary.dane()
+    c = NULL
+    finished = True
+    Dane.STOP = False
+    def ClickedConf1(self):
+        Clock.schedule_interval(self.Refresh, 1)
+        if(P1.finished==True):
+            if(P1.c==NULL):
+                P1.finished = False
+                P1.c = Thread(target=self.SearchForHosts)
+                #P1.c.daemon = True
+                P1.c.start()
+                
+                
+        else:
+            self.Dane.STOP = True
+            P1.c.join()
+            self.ids.scan_button.text = "Scan"
+            P1.c = NULL
+            P1.finished = True
+        
+        
+    def spinner_clicked(self, value):
+        print("Selected ip is " + value)
+        
+
+
+    def SearchForHosts(self):
+        net_info = self.Dane.get_network_info()
+        print(net_info)
+        result = self.Dane.scan_network_multi_thread(net_info[3],net_info[4])
+        print(result)
+        P1.finished = True
+        while(modules.auxilliary.dane.JOINED!=True):
+            sleep(0.1)
+        print("[INFO] succesfully closed threads")
+        modules.auxilliary.dane.JOINED=False
+        P1.c.join()
+        P1.c = NULL
+
+    def Refresh(self, interval):
+        if(P1.finished == False):
+            self.ids.scan_button.text = self.Dane.Percent
+            #print("r-"+self.Dane.Percent)
+            self.ids.spinner_id.values = self.Dane.HOSTS
+        else:
+            self.ids.scan_button.text = "Scan"
+
+      
+        
+        
+
+    
+
+class P2(FloatLayout):
+    def ClickedConf2(self):
+        print("button on conf 2")
+
+class P3(FloatLayout):
+    def ClickedConf3(self):
+        print("button on conf 3")
+
+class P4(FloatLayout):
+    def ClickedConf4(self):
+        print("button on conf 4")
+    
+
+def showPopUp1():
+    show = P1()
+    popupWindow = Popup(title="SYN FLOOD CONF", content=show, size_hint=(0.8,0.8) )
+    popupWindow.open()
+
+def showPopUp2():
+    show = P2()
+    popupWindow = Popup(title="App2", content=show, size_hint=(0.8,0.8) )
+    popupWindow.open()
+
+def showPopUp3():
+    show = P3()
+    popupWindow = Popup(title="App3", content=show, size_hint=(0.8,0.8) )
+    popupWindow.open()
+
+def showPopUp4():
+    show = P4()
+    popupWindow = Popup(title="App4", content=show, size_hint=(0.8,0.8) )
+    popupWindow.open()
+
+
+if __name__ == '__main__':
+    MyApp().run()
--- modules/auxilliary.py
+++ modules/auxilliary.py
@@ -7,6 +7,7 @@ import ipaddress
 import math
 from time import sleep
 from turtle import delay
+
 import netifaces
 
 from threading import Thread
@@ -15,151 +16,168 @@ from scapy.all import IP, ICMP, TCP, sr, sr1
 from netaddr import IPAddress
 
 
-
-target_hosts = []
-
-
-def get_network_info():
-
-    netifaces.gateways()
-    iface = netifaces.gateways()['default'][netifaces.AF_INET][1]
-    ip = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']
-    netmask =netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['netmask']
-    network = ipaddress.IPv4Network(ip+'/'+netmask, strict=False).network_address
-    iterator = math.pow(2,(32-IPAddress(netmask).netmask_bits()))-2
-
-    return iface,ip,netmask,network,iterator
+class dane:
+    target_hosts = []
+    HOSTS = []
+    Percent = "Scan"
+    STOP = False
+    JOINED = False
 
 
+    def get_network_info(self):
+
+        netifaces.gateways()
+        iface = netifaces.gateways()['default'][netifaces.AF_INET][1]
+        ip = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']
+        netmask =netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['netmask']
+        network = ipaddress.IPv4Network(ip+'/'+netmask, strict=False).network_address
+        iterator = math.pow(2,(32-IPAddress(netmask).netmask_bits()))-2
+
+        return iface,ip,netmask,network,iterator
 
 
-def send_ping_multi_thread(target_ip,hosts):
- 
-    resp = sr(IP(dst=target_ip)/ICMP(),timeout=1,verbose=0, multi= True)
-    answered = str(resp[0])
-    print(target_ip)
-    print(answered)
-    print(answered.split()[3].split(":",1)[1])
-    if(int(answered.split()[3].split(":",1)[1]) == 1):
-        hosts.append(target_ip)
 
 
-def scan_network_multi_thread(target_network, iterator):
-   
-    hosts = []
-    threads = []
- 
-    for i in range(0,int(iterator)):
-        target_ip = str(target_network + i)
-        thread = threading.Thread(target = send_ping_multi_thread,args = [target_ip,hosts])
-        sleep(0.2)
-        thread.start()
-        threads.append(thread)
- 
-    for t in threads:
-        t.join()
-
-    return hosts
+    def send_ping_multi_thread(self,target_ip,hosts):
     
-
-def send_ping(target_ip):
-
-    resp = sr(IP(dst=target_ip)/ICMP(),timeout=1,verbose=0)
-    answered = str(resp[0])
-    print(target_ip)
-    print(answered)
-    print(answered.split()[3].split(":",1)[1])
-    if(int(answered.split()[3].split(":",1)[1]) == 1):
-        return True
-
-    else:
-        return False
-
-
-
-def scan_network_single_thread(target_network, iterator):
-   
-    hosts = []
-
-    for i in range(0,int(iterator)):
-        target_ip = str(target_network+ i)
-        result = send_ping(target_ip)
-        if(result == True):
+        resp = sr(IP(dst=target_ip)/ICMP(),timeout=1,verbose=0, multi= True)
+        answered = str(resp[0])
+        #print(target_ip)
+        #print(answered)
+        #print(answered.split()[3].split(":",1)[1])
+        if(int(answered.split()[3].split(":",1)[1]) == 1):
+            print(target_ip + " is online")
             hosts.append(target_ip)
+            self.HOSTS.append(target_ip)
 
-    return hosts
 
 
-def scan_port_multi_thread(dst_ip, dst_port,ports):
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    socket.setdefaulttimeout(0.5)
+    def scan_network_multi_thread(self,target_network, iterator):
+        STOP = False
+        hosts = []
+        threads = []
+        Percent = ""
+        for i in range(0,int(iterator)):
+            if(STOP):
+                break
+            target_ip = str(target_network + i)
+            if(i%10==0):
+                self.Percent = "" + str(round(i/int(iterator)*100,1))+"%"
+                #print("t-"+Percent)
+            thread = threading.Thread(target = self.send_ping_multi_thread,args = [target_ip,hosts])
+            sleep(0.2)
+                
 
-    result = s.connect_ex((dst_ip,dst_port))
-    if(result == 0):
-        ports.append(dst_port)
-        print("Port " +  str(dst_port) + " found open")
-    else:
-        print("Port " +  str(dst_port) + " closed")
-            
+            thread.start()
+            threads.append(thread)
 
-def scan_host_ports_multi_thread(dst_ip):
-
-    ports = []
-    threads = []
-
-    for port in range(0,1024):
-        thread = threading.Thread(target = scan_port_multi_thread, args = [dst_ip,port,ports])
-        sleep(0.1)
-        thread.start()
-        threads.append(thread)
-
-    for t in threads:
-        t.join()
     
-    return ports
+        for t in threads:
+            t.join()
+        self.JOINED = True
+        self.Percent = "Scan"
+        self.STOP = False
+        return hosts
+        
 
+    def send_ping(self,target_ip):
 
-def scan_port(dst_ip, dst_port):
-    s_port = RandShort()
-    packet = IP(dst =dst_ip )/ TCP(sport = s_port, dport = dst_port, flags='S')
-    resp = sr1(packet,timeout=1,verbose=0)
-    if(str(type(resp))== "<type 'NoneType'>"):
-        print("Port " +  str(dst_port) + " closed")
-        return False
-    elif(resp.haslayer(TCP)):
-        if(resp.getlayer(TCP).flags == 0x12):
-            print("Port " +  str(dst_port) + " found open")
+        resp = sr(IP(dst=target_ip)/ICMP(),timeout=1,verbose=0)
+        answered = str(resp[0])
+        #print(target_ip)
+        #print(answered)
+        #print(answered.split()[3].split(":",1)[1])
+        if(int(answered.split()[3].split(":",1)[1]) == 1):
             return True
+        else:
+            return False
 
 
 
-
-def scan_host_ports_single_thread(dst_ip):
-    ports = []
-    for i in range(0,1023):
-        res = scan_port(dst_ip,i)
-        if(res == True):
-            ports.append(i)
+    def scan_network_single_thread(self,target_network, iterator):
     
-    return ports
+        hosts = []
+
+        for i in range(0,int(iterator)):
+            ProgressBarIterator = i
+            target_ip = str(target_network+ i)
+            result = send_ping(target_ip)
+            if(result == True):
+                hosts.append(target_ip)
+
+        return hosts
 
 
-if __name__ == '__main__':
-    net_info = get_network_info()
-    print(net_info)
-    #print(net_info[0])
-    #print(str(net_info[3]))
-    #print(str(net_info[4]))
+    def scan_port_multi_thread(self,dst_ip, dst_port,ports):
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
 
-    #result = scan_network_multi_thread(net_info[3],net_info[4])
-    #result  = scan_network_single_thread(net_info[3],net_info[4])
-    #print(result)
+        result = s.connect_ex((dst_ip,dst_port))
+        if(result == 0):
+            ports.append(dst_port)
+            print("Port " +  str(dst_port) + " found open")
+        #else:
+            #print("Port " +  str(dst_port) + " closed")
+                
 
-    #print(scan_port("192.168.0.31",135))
-    #print(scan_host_ports("192.168.0.31"))
-    res = []
-    res = scan_host_ports_multi_thread("192.168.0.31")
-    print(res)
+    def scan_host_ports_multi_thread(self,dst_ip):
+
+        ports = []
+        threads = []
+
+        for port in range(0,1024):
+            thread = threading.Thread(target = scan_port_multi_thread, args = [dst_ip,port,ports])
+            sleep(0.1)
+            thread.start()
+            threads.append(thread)
+
+        for t in threads:
+            t.join()
+        
+        return ports
+
+
+    def scan_port(self,dst_ip, dst_port):
+        s_port = RandShort()
+        packet = IP(dst =dst_ip )/ TCP(sport = s_port, dport = dst_port, flags='S')
+        resp = sr1(packet,timeout=1,verbose=0)
+        if(str(type(resp))== "<type 'NoneType'>"):
+            #print("Port " +  str(dst_port) + " closed")
+            return False
+        elif(resp.haslayer(TCP)):
+            if(resp.getlayer(TCP).flags == 0x12):
+                print("Port " +  str(dst_port) + " found open")
+                return True
+
+
+
+
+    def scan_host_ports_single_thread(self,dst_ip):
+        ports = []
+        for i in range(0,1023):
+            res = scan_port(dst_ip,i)
+            if(res == True):
+                ports.append(i)
+        
+        return ports
+
+
+    if __name__ == '__main__':
+        net_info = get_network_info()
+        print(net_info)
+        #print(net_info[0])
+        #print(str(net_info[3]))
+        #print(str(net_info[4]))
+
+        result = scan_network_multi_thread(net_info[3],net_info[4])
+        #result  = scan_network_single_thread(net_info[3],net_info[4])
+        print(result)
+
+        #print(scan_port("192.168.0.31",135))
+        #print(scan_host_ports("192.168.0.31"))
+        #res = []
+        #res = scan_host_ports_multi_thread("192.168.0.31")
+        #print(res)
    
 
     
